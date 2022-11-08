0deecd14adca53c0e508db6b9668ae1d9554cc2b
Jose Garcia <jose.garcia@criticaltechworks.com>
2022-11-08 14:38:19 +0000

    query params

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,7 +7,14 @@ router.get('/', ProductController.products_get_all)
 
 router.post('/',  checkAuth, ProductController.products_create_product)
 
-router.get('/:productId',  checkAuth, ProductController.product_get_product)
+router.get('/:productId',  checkAuth,  async function(req, res, next) {
+    const  uniqueCode = req.query.productId;
+    if (uniqueCode) {
+        ProductController.product_get_product_query
+     } else if (uniqueCode) {
+        ProductController.product_get_product
+     }
+})
 
 router.patch('/:productId', checkAuth,  ProductController.products_update_product)
 
